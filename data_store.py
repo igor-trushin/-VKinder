ad207d17f0be8be04656d1742f4f1080d86da244
Игорь Трушин <130861658+igor-trushin@users.noreply.github.com>
2023-06-22 14:48:33 +0300

    data_store.py

--- data_store.py
+++ data_store.py
@@ -10,6 +10,8 @@ from config import db_url_object
 metadata = MetaData()
 Base = declarative_base()
 
+Base.metadata.create_all(engine)
+
 
 class Viewed(Base):
     __tablename__ = 'viewed'
@@ -39,8 +41,8 @@ def check_user(engine, profile_id, worksheet_id):
 
 
 if __name__ == '__main__':
-
-    Base.metadata.create_all(engine)
+    engine = create_engine(db_url_object)
+    
     # add_user(engine, 2113, 654623)
     res = check_user(engine, 2113, 654623)
     print(res)
