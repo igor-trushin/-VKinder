5f9fac1fd850d3b5d54e7e36f5d0f12687f7b32d
Игорь Трушин <130861658+igor-trushin@users.noreply.github.com>
2023-06-22 13:51:06 +0300

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-comunity_token = ''
-access_token = ''
-db_url_object = ""
+comunity_token ='vk1.a.3BV49VcjqzURpG0aqUobJQnavS8u4Fq9BTTB8TvQ2MS4MU3EKUoqO-w'
+access_token ='vk1.a.VKb2gmWGLvU4LWk8tNAwofbmkSqA7qoh1e4V9KL'
+
+
+db_url_object ="postgresql://postgres:Deluxe123@localhost:5432/diplomvkinder"
